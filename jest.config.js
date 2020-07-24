4a98f2bd83e1537163d241e0232080bf5423e738
wenderpmachado <wenderpmachado@gmail.com>
2020-07-23 21:35:55 -0300

    chore: added jest and typescript configuration + dependencies

---
+++ jest.config.js
@@ -0,0 +1,46 @@
+// const { compilerOptions } = require('./tsconfig.json')
+// const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!**/tests/**'
+  ],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: [
+    'text',
+    'text-summary',
+    'lcov',
+  ],
+
+  // A preset that is used as a base for Jest's configuration
+  preset: '@shelf/jest-mongodb',
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+      '.+\\.ts$': 'ts-jest'
+  },
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  // moduleNameMapper: pathsToModuleNameMapper(
+  //   compilerOptions.paths,
+  //   { prefix: '<rootDir>/src/' }
+  // ),
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ['<rootDir>/tests'],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+};
